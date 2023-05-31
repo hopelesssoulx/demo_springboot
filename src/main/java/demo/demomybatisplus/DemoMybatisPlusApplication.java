063ff8a81874e9243728ac2679e103bdec0061dc
hopelesssoulx <hopelesssoulx@gmail.com>
2023-05-31 11:42:59 +0800

    init

---
+++ src/main/java/demo/demomybatisplus/DemoMybatisPlusApplication.java
@@ -0,0 +1,13 @@
+package demo.demomybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMybatisPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMybatisPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/demomybatisplus/DemoMybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.demomybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMybatisPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
